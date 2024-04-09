d7aadacb63e6a97ce811cfc7ccaf09639178904b
Jsobkowiak <jonathan.sobkowiak62@gmail.com>
2024-04-09 17:25:32 +0200

    CreateProgramme

--- lib/Services/ProgrammeService.dart
+++ lib/Services/ProgrammeService.dart
@@ -10,11 +10,25 @@ class ProgrammeService implements ProgrammeServiceInterface {
   Future<List<Programme>> getAllProgrammes() async {
     List<Programme> programmes = [];
     await db.collection("programmes").get().then((collection) => {
-      for(var doc in collection.docs){
-        programmes.add(Programme.fromDocument(doc))
-      }
-    });
+          for (var doc in collection.docs)
+            {programmes.add(Programme.fromDocument(doc))}
+        });
     return programmes;
+  }
+
+  Future<void> PostProgrammes(Programme programme) async {
+    final progr = <String, dynamic>{
+      "estPublic": "Ada",
+      "id_utilisateur": "Lovelace",
+      "nom": 1815,
+      "rep": 1815
+    };
+
+    this
+        .db
+        .collection("programmes")
+        .add(progr)
+        .then((value) => print("Add successfull"));
   }
 
   @override
