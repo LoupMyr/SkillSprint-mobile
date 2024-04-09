f50d9c74859691f0d7fdae66e2ae7d2216a0f880
Jsobkowiak <jonathan.sobkowiak62@gmail.com>
2024-04-09 10:10:21 +0200

    Pages

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:skillsprint/Domain/Services/AuthServiceInterface.dart';
+import 'package:skillsprint/Layouts/Accueil.dart';
 import 'package:skillsprint/Layouts/AppBarLayout.dart';
+import 'package:skillsprint/Layouts/CreateProgrammes.dart';
+import 'package:skillsprint/Layouts/Profil.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title, required this.authService});
@@ -11,20 +14,50 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-
-
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  final List<Widget> _widgetOptions = <Widget>[
+    Accueil(),
+    CreateProgrammes(),
+    Profil()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarLayout(title: "Accueil", authService: widget.authService),
-      body: const Center(
-        child:
-          Column(
-            children: [
-              Text("HomePage")
-            ],
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
           ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+            backgroundColor: Colors.purple,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
